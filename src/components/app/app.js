ac942283ca9debb1d1aa9fbfc678d855be9bf0b6
Ramil <vk2011@bk.ru>
2021-09-29 13:37:46 +0500

    add increase and raising states

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,13 +5,53 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
+/* import { Component } from 'react';
+
+class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 27,
+            text: '+++',
+            position: ''
+        }
+    }
+
+    nextYear = () => {
+        this.setState(state => ({
+            years: state.years + 1
+        }))
+    }
+
+    commitInputChanges = (e) => {
+        this.setState({
+            position: e.target.value
+        })
+    }
+
+    render() {
+        const {name, surname, link} = this.props;
+        const {years, text, position} = this.state;
+        return (
+            <div>
+                <button onClick={this.nextYear}>{text}</button>
+                <h1>My name is {name}, surname - {surname}, age - {years}, position -  {position}</h1>
+                <a href={link}>My profile</a>
+                <form>
+                    <span>Введите должность</span>
+                    <input type="text" onChange={this.commitInputChanges} />
+                </form>
+            </div>
+        )
+    }
+} */
 
 function App() {
 
     const data = [
-        {name: 'John S.', salary: 800, increase: true},
-        {name: 'Alex M.', salary: 3000, increase: false},
-        {name: 'Carl W.', salary: 5000, increase: true}
+        {name: 'John S.', salary: 800, increase: true, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: false, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: true, id: 3}
     ];
 
     return (
